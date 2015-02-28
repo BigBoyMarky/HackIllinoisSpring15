371a4db5c6cf8c327e71c0bb9a5cc69d15f6c4cd
Eleanor Eason <e.eason@gatech.edu>
2015-02-28 09:41:15 -0500

    2nd try?

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-var settingVisibility = false;
+// var settingVisibility = false;
 
-function showSettings() {
-    if (settingVisibility) {
-        document.getElementById("settingspanel").style.display = "none";
-        settingVisibility = false;
-    } else {
-        document.getElementById("settingspanel").style.display = "block";
-        settingVisibility = true;
+// function showSettings() {
+//     if (settingVisibility) {
+//         document.getElementById("settingspanel").style.display = "none";
+//         settingVisibility = false;
+//     } else {
+//         document.getElementById("settingspanel").style.display = "block";
+//         settingVisibility = true;
+//     }
+// }
+
+function showSettings() { 
+    if (document.getElementById("settingspanel").style.display==="block") {
+        document.getElementById("settingspanel").style.display = "none"; 
+    } else { 
+        $("#settingspanel").slideDown(450);; 
     }
 }
